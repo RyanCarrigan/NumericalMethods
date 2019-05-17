fad1b8b9ec46d83e0b292d45fe55ffdd113fd76d
Ryan Carrigan <ryanthecarrigan@gmail.com>
2019-05-17 11:39:36 -0600

    Delete Machine_epsilon.m

--- Machine_epsilon.m
+++
@@ -1,6 +0,0 @@
-machine_eps = 1; %creating epsilon
-while 1 + (machine_eps/2) > 1 %loop operates until less than 1
-    machine_eps = machine_eps / 2; %continually gets smaller
-end
-fprintf( 'Machine epsilon is %s\n', machine_eps) %printing calculated value
-fprintf('The value for machine epsilon the computer has is %s',eps) %showing computers value
