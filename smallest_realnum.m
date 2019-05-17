dd60450c4da213210106c27946b49c27712eeedc
Ryan Carrigan <ryanthecarrigan@gmail.com>
2019-05-17 11:40:05 -0600

    Delete smallest_realnum.m

--- smallest_realnum.m
+++
@@ -1,8 +0,0 @@
-n=1; %creating n
-while n-0> 0 %having it as n is greater than 0 so that when it dips below the while loop stops.
-    n=n/2; %continually gets smaller
-    if n/2-0==0 %determining if number is equal to 0
-        disp(n) %showing the number
-        break %just incase this goes to long
-    end
-end
